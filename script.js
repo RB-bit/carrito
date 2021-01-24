7643a14881df1c446420e72b8c99e617fed7232e
RB-bit <rodrigobossi097@gmail.com>
2021-01-24 19:28:24 -0300

    creación de repo

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+//  function ingresarAuto(){
+
+//     var auto = prompt("Ingrese una la marca de su auto");    
+//     var modelo = prompt("Ingrese el modelo");
+//     var anio = parseInt(prompt("Ingrese el año de su auto"));
+
+//         var datosAuto = (auto + modelo + anio)
+    
+//      alert(datosAuto);
+//  }
+
+//  ingresarAuto();
+
+ 
+    // function Auto(marca, anio, modelo, kilometros){
+    //     this.marca = marca;
+    //     this.anio = anio;
+    //     this.modelo = modelo;
+    //     this.kilometros = kilometros;
+    // }
+
+ 
+    // for(var i=0;i<3;i++){
+    //     var marca = prompt("Ingrese la marca de su auto");
+    //     var anio = parseInt(prompt("Ingrese el año de su auto"));
+    //     var modelo = prompt("Ahora ingrese el modelo");
+    //     var kilometros = parseInt(prompt("Y por último los kilómetros"));
+
+    //     var miAuto = new Auto(marca, anio, modelo, kilometros);
+
+
+    //     console.log(miAuto);
+
+    // }
+
+    // Arrays
+
+    let empresasCreditos = ["San Cristobal", "La Segunda", "La Caja", "Rio Uruguay", "Iunigo"];
+
+    for(let i=0; i < empresasCreditos.length; i++){
+        console.log(empresasCreditos[i])
+    }
+
+    
