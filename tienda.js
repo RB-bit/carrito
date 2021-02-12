b8baa3957886e964b5cd64bde2b21a887471b4d1
RB-bit <rodrigobossi097@gmail.com>
2021-02-12 11:59:13 -0300

    Creación de cards de productos en DOM

--- tienda.js
+++ tienda.js
@@ -1,60 +1,87 @@
 
 
-
-// Selecciono la clase del botón de cada card y/o producto
-let addToShoppingCartButtons = document.querySelectorAll ('.addToCart');
-
-//creo el evento click en la variable anterior y recorro todos esas clases con un forEach
-addToShoppingCartButtons.forEach(addToCartButton => {
-    addToCartButton.addEventListener('click', addToCartClicked)
-});
-
-let shoppingCartItemsContainer = document.querySelector (
-    'shoppingCartItemsContainer'
-);
-
-// Tomo el evento anterior, lo guardo en una variable y luego traigo todos los items cercanos con el método closest
-function addToCartClicked (event) {
-    let button = event.target;
-    let item = button.closest('.item');
-
-// Selecciono cuáles son los items que quiero y que contenido de cada uno
-    let itemTitle = item.querySelector ('.item-title').textContent;
-    let itemPrice = item.querySelector ('.item-price').textContent;
-    let itemImage = item.querySelector ('.item-image').src;
-
-    addItemToShoopingCart (itemTitle, itemPrice, itemImage);
+function zapatilla (marca, modelo, talle, precio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.talle = talle;
+    this.precio = precio;
 }
 
-function addItemToShoopingCart(itemTitle, itemPrice, itemImage) {
-   let shoppingCartRow = document.createElement('div');
-   let shoppingCartContent = `
-   <div class="row shoppingCartItem">
-        <div class="col-6">
-        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <img src=${itemImage} class="shopping-cart-image">
-            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
-            </h6>
-        </div>
-        </div>
-        <div class="col-2">
-        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-        </div>
-        </div>
-        <div class="col-4">
-        <div
-            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-            <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                value="1">
-            <button class="btn btn-danger buttonDelete" type="button">X</button>
-        </div>
-        </div>
-    </div>`;
-    shoppingCartRow.innerHTML = shoppingCartContent;
-    shoppingCartItemsContainer.append(shoppingCartRow);
-    console.log(shoppingCartContent)
+const stockProduct = [   
+    new zapatilla ("Nike", "Air Jordan", 43, "$500"),
+    new zapatilla  ("Adidas", "Sport", 44, "$450"),
+    new zapatilla ("Puma", "Borussia", 42, "$400"),
+    new zapatilla ("Vans", "Old School", 41, "$550"),
+    new zapatilla ("Reebok", "Royal", 42, "$450"),
+    new zapatilla ("Vans", "Eva", 43, "475"),
+];
+
+function mostrarStock(){
+    for (const zapas of stockProduct){
+        let zapasList = document.getElementById ('store');
+        zapasList.className = "productos"
+        let elementoHtml = document.createElement ('article');
+        elementoHtml.innerHTML = `
+
+                <div class="col-md-4">
+                <div class="item shadow mb-4">
+                    <h3 class="item-title">${zapatilla.marca}</h3>
+                    <img class="item-image" src="./img/airJordan.jpeg">
+                        <div class="item-details">
+                            <h4 class="item-price">${zapatilla.precio}</h4>
+                            <button class="item-button btn btn-primary addToCart">AÑADIR AL CARRITO</button>
+                        </div>
+                    </div>
+                </div>
+
+        `;
+        zapasList.appendChild(elementoHtml)}
 }
+
+mostrarStock();
+// for (const zapas of stockProduct){
+//     let zapasList = document.getElementById ('store');
+//     let elementoHtml = document.createElement ('article');
+//     elementoHtml.innerHTML = `<div class="col-12 col-md-3">
+//     <div class="item shadow mb-4">
+//         <h3 class="item-title">${zapatilla.marca}</h3>
+//         <img class="item-image" src="./img/airJordan.jpeg">
+//             <div class="item-details">
+//                 <h4 class="item-price">${zapatilla.precio}</h4>
+//                 <button class="item-button btn btn-primary addToCart">AÑADIR AL CARRITO</button>
+//             </div>
+//         </div>
+//     </div>`;
+//     zapasList.appendChild(elementoHtml)
+// }
+
+
+// zapasList.appendChild(fragment)
+
+// function mostrarStock(){
+//     stockProduct.forEach((stockProduct) => {
+//        var stockHTML = document.createElement("article")
+//         stockHTML.className = 'productos';
+
+//         stockHTML.innerHTML = `
+//         <div class="col-12 col-md-4">
+//                 <div class="item shadow mb-4">
+//                     <h3 class="item-title">${zapatilla.marca}</h3>
+//                     <img class="item-image" src="./img/airJordan.jpeg">
+
+//                     <div class="item-details">
+//                         <h4 class="item-price">${zapatilla.precio}</h4>
+//                         <button class="item-button btn btn-primary addToCart">AÑADIR AL CARRITO</button>
+//                     </div>
+//                 </div>
+//         </div>
+//         `;
+        
+//         document.getElementById("store").parent.appendChild(stockHTML);
+//     });
+// }
+
+
 
 
 
